8590454aed7ac13818cf37ce4bb5d965a12f8c14
Tim Nguyen <tiim@berkeley.edu>
2026-09-21 13:40:06 -0700

    final images at different sampling rates

--- stitching/manual/main.py
+++ stitching/manual/main.py
@@ -4,21 +4,27 @@ import stitch
 import utils
 
 video_path = "../stitch_in.mp4"
-sample_rate = 1 # Hz
 out_dir = "out"
 
-video = cv2.VideoCapture(video_path)
-if not video.isOpened():
-    raise SystemExit("Could not open video")
 
-out_path = utils.make_unique_dir(out_dir, "stitch", identifier=utils.id_type.DATETIME)
 
-fps = video.get(cv2.CAP_PROP_FPS)
-diff = max(int(fps / sample_rate), 1)
-frames = stitch.video_to_frame_array(video, step=diff)
-video.release()
+for i in range (5):
+    video = cv2.VideoCapture(video_path)
 
-offsets  = stitch.calculate_relative_offsets_ncc(stitch.to_grayscale(frames))
-stitched = stitch.stitch_frames_from_offset(frames, offsets)
+    if not video.isOpened():
+        raise SystemExit("Could not open video")
+    sample_rate = 1/(2**i)
+    out_path = utils.make_unique_dir(out_dir, "stitch", identifier=utils.id_type.DATETIME)
 
-skio.imsave(out_path / "stitch.png", stitched)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    diff = max(int(fps / sample_rate), 1)
+    frames = stitch.video_to_frame_array(video, step=diff)
+    video.release()
+
+    offsets  = stitch.calculate_relative_offsets_ncc(stitch.to_grayscale(frames))
+    stitched = stitch.stitch_frames_from_offset(frames, offsets)
+
+
+    imname_str = "stitch"+ str(sample_rate) + "hz.png"
+
+    skio.imsave(out_path / imname_str , stitched)
